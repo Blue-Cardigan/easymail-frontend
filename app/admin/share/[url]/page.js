fd4f6bf4a2ae068585b50bda12309e5dc328b428
Red-Cardigan <jethro.reeve@gmail.com>
2024-08-14 21:29:44 +0100

    Changed shareable link

--- app/admin/share/[url]/page.js
+++ app/admin/share/[url]/page.js
@@ -5,7 +5,7 @@ import ClientResponse from '@/components/ClientResponse'
 
 export default function ShareCampaignPage() {
   const params = useParams()
-  const url = `easymail.com/${params.url}`
+  const url = `easymail.civita.co.uk/${params.url}`
 
   return (
     <div className="min-h-screen bg-background flex flex-col items-center justify-center">
