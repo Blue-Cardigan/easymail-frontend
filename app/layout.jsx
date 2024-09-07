2b7a7c347a53ba7e4a5de0e9b42d22d37ed337c2
robertvava <vavarobert10@gmail.com>
2024-09-07 15:12:06 +0100

    added favicon, finally!

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export const metadata = {
     icon: [
       { url: '/favicon.ico', sizes: '32x32' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }, 
       { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
       { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
     ],
